26b9f1fc133d504782ebe656e2c88a15fdb354a3
Yi Xin Tan <ytan0311@student.monash.edu>
2025-09-19 14:34:14 +1000

    Initial Commit

---
+++ per-unit/per_unitApp.swift
@@ -0,0 +1,17 @@
+//
+//  per_unitApp.swift
+//  per-unit
+//
+//  Created by yixin on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct per_unitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
